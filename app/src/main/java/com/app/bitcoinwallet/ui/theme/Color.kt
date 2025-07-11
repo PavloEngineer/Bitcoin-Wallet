4a2448d08d4fe657d9355c3457f63665560a8329
PavloEngineer <kokhanevych.pavlo@gmail.com>
2025-07-11 13:32:34 +0300

    chore: setup themes and colors, typography

--- app/src/main/java/com/app/bitcoinwallet/ui/theme/Color.kt
+++ app/src/main/java/com/app/bitcoinwallet/ui/theme/Color.kt
@@ -1,11 +1,22 @@
 package com.app.bitcoinwallet.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val GREEN = Color(0xFF356C12)
+val RED = Color(0xFFD50000)
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey70 = Color(0xFF412B73)
 val Pink40 = Color(0xFF7D5260)
+
+val Pink60 = Color(0xFFCDD0F1)
+val Blue40 = Color(0xFF24273A)
+val Blue60 = Color(0xFF353A4E)
+val PinkLight = Color(0xFFB6BCF7)
+val MixedBackgroundBlue = lerp(Blue40, Blue60, 0.5f)
--- app/src/main/java/com/app/bitcoinwallet/ui/theme/Theme.kt
+++ app/src/main/java/com/app/bitcoinwallet/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.app.bitcoinwallet.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,25 +11,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary =  Pink60,
+    secondary = PurpleGrey70,
+    tertiary = PurpleGrey40,
+    background = MixedBackgroundBlue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary =  Pink60,
+    secondary = PurpleGrey70,
+    tertiary = PurpleGrey40,
+    background = MixedBackgroundBlue
 )
 
 @Composable
@@ -52,7 +43,7 @@ fun BitcoinWalletTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }
